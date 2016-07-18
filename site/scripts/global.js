4ac13c1e47c864f6bf09aad33fb213ba237d0b19
will ruby <me@willruby.com>
2016-07-17 20:00:40 -0700

    maybe

--- site/scripts/global.js
+++ site/scripts/global.js
@@ -155,7 +155,7 @@ function loadImage(el,source,id){
 				$iframe.remove();
 			}
 			img.src = source;
-			if(source.find('gif') != -1) img.onload()
+			if(source.search('gif') != -1) img.onload()
 		} else {
 			if($iframe) $iframe.remove();
 			$hero.append('<iframe src="http://player.vimeo.com/video/'+id+'" width="100%" height="100%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
